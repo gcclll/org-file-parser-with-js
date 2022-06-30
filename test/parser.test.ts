79348d9ad8a698207511b9e341017cb5b3686437
gcclll <gccll.love@gmail.com>
2022-06-30 23:19:16 +0800

    add: tests

---
+++ test/parser.test.ts
@@ -0,0 +1,12 @@
+
+import { baseParse as parse } from '../dist/'
+
+describe('parser', () => {
+  it('header', () => {
+    expect(parse(`* header 1`)).toMatchObject([{
+      type: 3 /*NodeTypes.HEADER*/,
+      content: `* header 1`,
+      title: `header 1`
+    }])
+  })
+})
